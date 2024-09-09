e735afa6def73591fd9c9c554eda86dea61cd34d
misterdown <misterdown@mail.ru>
2024-09-09 21:16:39 +0500

    fix

--- tests.cpp
+++ tests.cpp
@@ -9,12 +9,12 @@ void default_test() {
     std::cout << "default test\n";
 
     const std::string data(
-    "std::wstring datal(data.begin(), data.end());\n"
+    "std::wstring datal(data.begin(), data.end());\n\"\""
     "sxt::tokenizer<std::wstring> tokenizer(datal.begin(), datal.end());\n"
     );
     sxt::tokenizer<std::string> tokenizer(data.begin(), data.end());
     while (true) {
-        const sxt::token_new<std::string> tk = tokenizer.next_new_token(sxt::STX_EXT_TOKEN_TYPE_FLAG_BIT_NONE);
+        const sxt::value_token<std::string> tk = tokenizer.next_new_token(sxt::STX_EXT_TOKEN_TYPE_FLAG_BIT_NONE);
         if (!tk.is_valid())
             break;
         std::cout << token_type_to_string(tk.type()) << ":  " << tk.value() <<  " \n";
@@ -29,7 +29,7 @@ void wchar_test() {
     );
     sxt::tokenizer<std::wstring> tokenizer(data.begin(), data.end());
     while (true) {
-        const sxt::token_new<std::wstring> tk = tokenizer.next_new_token(sxt::STX_EXT_TOKEN_TYPE_FLAG_BIT_NONE);
+        const sxt::value_token<std::wstring> tk = tokenizer.next_new_token(sxt::STX_EXT_TOKEN_TYPE_FLAG_BIT_NONE);
         if (!tk.is_valid())
             break;
         std::wcout << token_type_to_wstring(tk.type()) << L":  " << tk.value() <<  L" \n";
@@ -44,7 +44,7 @@ void string_letterals_test() {
     );
     sxt::tokenizer<std::string> tokenizer(data.begin(), data.end());
     while (true) {
-        const sxt::token_new<std::string> tk = tokenizer.next_new_token(sxt::STX_EXT_TOKEN_TYPE_FLAG_BIT_STRING_LETTERAL);
+        const sxt::value_token<std::string> tk = tokenizer.next_new_token(sxt::STX_EXT_TOKEN_TYPE_FLAG_BIT_STRING_LETTERAL);
         if (!tk.is_valid())
             break;
         std::cout << token_type_to_string(tk.type()) << ":  " << tk.value() <<  " \n";
@@ -53,18 +53,18 @@ void string_letterals_test() {
 
 void perfomance_test() {
     std::cout << "default perfomance test\n";
-    
+
     const std::string data(
     "std::wstring datal = L\"greate greate greate GREATE!!!!! \\\"not greate really\\\"\";\n"
     "sxt::tokenizer<std::wstring> tokenizer(datal.begin(), datal.end());\n"
-    "sxt::token_new<std::wstring> tk = tokenizer.next_new_token(sxt::STX_EXT_TOKEN_TYPE_FLAG_BIT_STRING_LETTERAL);\n"
+    "sxt::value_token<std::wstring> tk = tokenizer.next_new_token(sxt::STX_EXT_TOKEN_TYPE_FLAG_BIT_STRING_LETTERAL);\n"
     );
     for (size_t e = 0; e < 5; ++e) {
         const auto start = std::chrono::steady_clock::now();
         for (size_t i = 0; i < 500000; ++i) {
             sxt::tokenizer<std::string> tokenizer(data.begin(), data.end());
             while (true) {
-                const sxt::token_new<std::string> tk = tokenizer.next_new_token(sxt::STX_EXT_TOKEN_TYPE_FLAG_BIT_STRING_LETTERAL);
+                const sxt::value_token<std::string> tk = tokenizer.next_new_token(sxt::STX_EXT_TOKEN_TYPE_FLAG_BIT_STRING_LETTERAL);
                 if (!tk.is_valid())
                     break;
             }
@@ -79,7 +79,7 @@ void perfomance_view_test() { //speed up x4 on O0 and ~x2 on O3
     const std::string data(
     "std::wstring datal = L\"greate greate greate GREATE!!!!! \\\"not greate really\\\"\";\n"
     "sxt::tokenizer<std::wstring> tokenizer(datal.begin(), datal.end());\n"
-    "sxt::token_new<std::wstring> tk = tokenizer.next_new_token(sxt::STX_EXT_TOKEN_TYPE_FLAG_BIT_STRING_LETTERAL);\n"
+    "sxt::value_token<std::wstring> tk = tokenizer.next_new_token(sxt::STX_EXT_TOKEN_TYPE_FLAG_BIT_STRING_LETTERAL);\n"
     );
     const std::string_view view = data;
     for (size_t e = 0; e < 5; ++e) {
@@ -87,7 +87,7 @@ void perfomance_view_test() { //speed up x4 on O0 and ~x2 on O3
         for (size_t i = 0; i < 500000; ++i) {
             sxt::tokenizer<std::string_view> tokenizer(view.begin(), view.end());
             while (true) {
-                const sxt::token_new<std::string_view> tk = tokenizer.next_new_token(sxt::STX_EXT_TOKEN_TYPE_FLAG_BIT_STRING_LETTERAL);
+                const sxt::value_token<std::string_view> tk = tokenizer.next_new_token(sxt::STX_EXT_TOKEN_TYPE_FLAG_BIT_STRING_LETTERAL);
                 if (!tk.is_valid())
                     break;
             }
@@ -104,6 +104,7 @@ void perfomance_view_test() {
 int main() {
     default_test();
     wchar_test();
+    string_letterals_test();
     perfomance_test();
     perfomance_view_test();
 
